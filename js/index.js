db3a5796e36b53b7ff60fd8a096554a797eb46e1
KuchmaSergey <71453623+KuchmaSergey@users.noreply.github.com>
2021-01-08 20:42:08 +0200

    решение задач первого модуля

---
+++ js/index.js
@@ -0,0 +1,99 @@
+// 5
+// // Базовый код
+// const productName = 'Дроид';
+// const pricePerItem = 3500;
+
+// // Пиши код ниже этой строки
+// const message = `Вы выбрали ${productName}, цена за штуку ${pricePerItem} кредитов`;
+// console.log(message);
+
+
+// 6
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = (pricePerDroid * orderedQuantity + deliveryFee);
+// const message = `Вы заказали дроидов на сумму ${totalPrice} кредитов.Доставка (${deliveryFee} кредитов) включена в сумму заказа.`;
+// console.log(message);
+// 7 задача
+// // Пиши код ниже этой строки
+// function sayHi () {
+//     console.log ('Привет, это моя первая функция!')
+//   }
+//   sayHi();
+
+
+// 8 задача
+// Пиши код ниже этой строки
+// function add(a,b,c) {
+//     console.log(`Результат умножения равен ${a+b+c}`);
+//     // Пиши код выше этой строки//
+//   }
+//   add(15, 27, 10);
+//   add(10, 20, 30);
+//   add(5, 10, 15);
+
+
+// 9 задача
+// function add(a, b, c) {
+//     //  Пиши код ниже этой строки
+//     return a+b+c   
+//   //  Пиши код выше этой строки
+//   }
+//   add(2, 5, 8); // 15
+//   console.log(add(15, 27, 10));
+//   console.log(add(10, 20, 30));
+//   console.log(add(5, 10, 15));
+
+// 10 задача
+// function makeMessage (name, price) {
+//     // Пиши код ниже этой строки
+//      const message = `Вы выбрали ${name}, цена за штуку ${price} кредитов`;
+//     // Пиши код выше этой строки
+//     return message;
+//   };
+
+// 11 задача
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//     // Пиши код ниже этой строки
+//     const totalPrice = (orderedQuantity*pricePerItem);
+  
+//     // Пиши код выше этой строки
+//     return totalPrice;
+//   };
+
+
+
+// 12 задача
+//   function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//     // Пиши код ниже этой строки
+//     const message = `Вы заказали дроидов на сумму ${orderedQuantity * pricePerDroid + deliveryFee} кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`
+//     // Пиши код выше этой строки
+//     return message;
+//   }
+
+// 13 задача
+// function isAdult(age) {
+//     // Пиши код ниже этой строки
+//     const passed = age >= 18;
+    
+  
+//     // Пиши код выше этой строки
+//     return passed;
+//   }
+
+
+// 14 задача
+
+
+
+
+// 15 задача
+
+
+// 16 задача
+
+
+// 17 задача
+
+
