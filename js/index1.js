6ff3e5b7bffc780834edd9abb8726426634a88e2
KuchmaSergey <71453623+KuchmaSergey@users.noreply.github.com>
2021-01-14 17:04:39 +0200

    решение задач модуль 2

--- js/index1.js
+++ js/index1.js
@@ -280,14 +280,136 @@
 //   }
 
 // задача 21
+// Напиши фукцнию findLongestWord(string) которая принимает произвольную строку состоящую только из слов разделённых пробелом (параметр string) и возвращает самое длинное слово в этой строке.
 
+// Тесты
+// Объявлена функция findLongestWord(string).
+// Вызов функции findLongestWord('The quick brown fox  jumped over the lazy dog') возвращает jumped.
+// Вызов функции findLongestWord('Google do a roll') возвращает Google.
+// Вызов функции findLongestWord('May the force be with you') возвращает force.
+// Вызов функции findLongestWord() со случайной строкой возвращает правильное значение.
+
+
+// function findLongestWord(string) {
+//   // Пиши код ниже этой строки
+//   let arr = string.split(' ');
+//    let countLetters = [];
+
+//     for (let i=0; i < arr.length; i += 1) {
+//         countLetters.splice(arr.length - 1, 0, arr[i].length);
+//     }
+//    let maxCountLetters = Math.max(...countLetters);
+//   let maxCountLettersIndex = countLetters.indexOf(maxCountLetters);
+//   return arr[maxCountLettersIndex];
+
+//   // Пиши код выше этой строки
+// }
 
 
 // задача 22
+// Дополни код функции createArrayOfNumbers(min, nax) так, чтобы она возвращала массив всех целых чисел от значения min до max.
+
+// Тесты
+// Объявлена функция createArrayOfNumbers(min, max).
+// Вызов функции createArrayOfNumbers(1, 3) возвращает [1, 2, 3].
+// Вызов функции createArrayOfNumbers(14, 17) возвращает [14, 15, 16, 17].
+// Вызов функции createArrayOfNumbers(29, 34) возвращает [29, 30, 31, 32, 33, 34].
+// Вызов функции createArrayOfNumbers() со случайными min и max возвращает правильный массив.
+// В цикле for использовался метод push.
+
+// function createArrayOfNumbers(min, max) {
+//     const numbers = [];
+//     // Пиши код ниже этой строки
+//   for (let i = min; i<= max; i+=1) {
+//     numbers.push(i);
+//   }
+//     // Пиши код выше этой строки
+//     return numbers;
+//   }
 // задача 23 
+// Напиши функцию filterArray(numbers, value), которая принимает массив чисел (параметр numbers) и возвращает новый массив, в котором будут только те элементы массива numbers, которые больше чем значение параметра value (число).
+
+// Тесты
+// Объявлена функция filterArray(numbers, value).
+// Вызов функции filterArray([1, 2, 3, 4, 5], 3) возвращает [4, 5].
+// Вызов функции filterArray([1, 2, 3, 4, 5], 4) возвращает [5].
+// Вызов функции filterArray([1, 2, 3, 4, 5], 5) возвращает [].
+// Вызов функции filterArray([12, 24, 8, 41, 76], 38) возвращает [41, 76].
+// Вызов функции filterArray([12, 24, 8, 41, 76], 20) возвращает [24, 41, 76].
+// Вызов функции filterArray() со случайным массивом и числом возвращает правильный массив.
+// В цикле for использовался метод push.
+
+
+
+// function filterArray(numbers, value) {
+//     // Пиши код ниже этой строки
+//   const array = [];
+//     for (let i = 0; i < numbers.length; i+=1) {
+     
+//       if ( numbers[i] > value) {
+//        array.push(numbers[i]); }
+//     }  
+//     return array;
+//     // Пиши код выше этой строки
+//   }
+
+
+
 // задача 24 
+// Функция checkFruit(fruit) принимает строку с названием фрукта (параметр fruit), и проверяет есть ли такой фрукт в массиве fruits.
+
+// Дополни код функции так, что если:
+
+// фрукт есть в массиве, то функция возвращает true;
+// фрукта нет в массиве, то функция возвращает false.
+// Тесты
+// Объявлена функция checkFruit(fruit).
+// Вызов checkFruit('слива') возвращает true.
+// Вызов checkFruit('мандарин') возвращает false.
+// Вызов checkFruit('груша') возвращает true.
+// Вызов checkFruit('Груша') возвращает false.
+// Вызов checkFruit('яблоко') возвращает true.
+// Вызов функции checkFruit() со случайным словом возвращает правильное значение boolean.
+// В функции использовался метод includes.
+
+
+// function checkFruit(fruit) {
+//     const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+  
+//     return fruits.includes(fruit) ; // Дополни эту строку
+//   }
+
+
 // задача 25
+// Общими элементами массивов называют те элементы, которые присутствуют во всех массивах.
+
+// Например, в двух массивах [1, 3, 5] и [0, 8, 5, 3] общими будут числа 3 и 5, т.к. они присутствуют в обоих исходных массивах. А числа 0, 1 и 8 присутствуют только в одном из массивов.
+
+// Напиши функцию getCommonElements(array1, array2) которая получает два массива произвольной длины в параметры array1 и array2, и возвращает новый массив, состоящий из тех элементов, которые присутствуют в обоих исходных массивах.
+
+// Тесты
+// Объявлена функция checkFruit(fruit).
+// Вызов getCommonElements([1, 2, 3], [2, 4]) возвращает [2].
+// Вызов getCommonElements([1, 2, 3], [2, 1, 17, 19]) возвращает [1, 2].
+// Вызов getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) возвращает [12, 27, 3].
+// Вызов getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) возвращает [10, 30, 40].
+// Вызов getCommonElements([1, 2, 3], [10, 20, 30]) возвращает [].
+// Вызов функции getCommonElements() со случайными двумя массивами возвращает правильный массив.
+// В цикле for использовались методы includes и push.
+
+// function getCommonElements(array1, array2) {
+//     // Пиши код ниже этой строки
+//   const newElement=[];
+//     for (const number of array1) {
+//       if(array2.includes(number)) {
+//         newElement.push(number)
+//       }
+//     }
+//   return newElement; 
+//     // Пиши код выше этой строки
+//   }
 // задача 26
+
 // задача 27
 // задача 28
 // задача 29
