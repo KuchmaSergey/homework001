3685c6514818e039d2c5a3d3f2d40fa55e6eb758
KuchmaSergey <71453623+KuchmaSergey@users.noreply.github.com>
2021-01-31 21:13:33 +0200

    решает задачи м5

--- js/index.m5.js
+++ js/index.m5.js
@@ -149,19 +149,113 @@
 
 
 
+  
 
 
 
-
-
-
-
-
+// function Car (brand, model, price) {
+//     this.brand= brand;
+//     this.model= model;
+//     this.price= price;
+//   };
 
 
 // задача 4
+
+
+// Задание
+// Выполни рефакторинг функции-конструктора Car так, чтобы она принимала один параметр - объект со свойсвами brand, model и price. Деструктуризируй объект в сигнатуре (подписи) функции.
+
+// Тесты
+// Объявлена функция Car({ brand, model, price }).
+// В результате вызова new Car({ brand: 'Audi', model: 'Q3', price: 36000 }) получится объект { brand: 'Audi', model: 'Q3', price: 36000 }.
+// В результате вызова new Car({ brand: 'BMW', model: 'X5', price: 58900 }) получится объект { brand: 'BMW', model: 'X5', price: 58900 }.
+// В результате вызова new Car({ brand: 'Nissan', model: 'Murano', price: 31700 }) получится объект { brand: 'Nissan', model: 'Murano', price: 31700 }.
+
+
+
+// function Car(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+  
+
+
+
+
+
+
+// function Car({brand, model, price}) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   };
+
+
 // задача 5
+// Задание
+// Добавь в свойство prototype функции-конструктора Car два метода:
+
+// getPrice() - возвращает значение свойства price из объекта который его будет вызывать.
+// changePrice(newPrice) - обновляет значение свойства price у объекта который его будет вызывать на newPrice.
+// Тесты
+// Объявлена функция Car({ brand, model, price }).
+// Вызов Car.prototype.hasOwnProperty('getPrice') возвращает true.
+// Значение Car.prototype.getPrice это функция.
+// Вызов Car.prototype.hasOwnProperty('changePrice') возвращает true.
+// Значение Car.prototype.changePrice это функция.
+// У объекта, созданного вызовом new Car({ brand: 'Audi', model: 'Q3', price: 36000 }), вызов метода getPrice() вернет число 36000.
+// У объекта, созданного вызовом new Car({ brand: 'Audi', model: 'Q3', price: 36000 }), вызов метода changePrice(35000) и последующем вызове getPrice() вернет число 35000.
+
+
+// function Car({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+
+
+
+
+// function Car({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   Car.prototype.getPrice = function () {
+//     return this.price;
+//   };
+//   Car.prototype.changePrice = function (newPrice) {
+//     this.price = newPrice;
+//   }
+
 // задача 6
+
+// function User({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+  
+//   User.prototype.getEmail = function () {
+//     return this.email;
+//   };
+  
+//   User.prototype.changeEmail = function (newEmail) {
+//     this.email = newEmail;
+//   };
+  
+//   const mango = new User({ name: 'Манго', email: 'mango@mail.com' });
+  
+//   console.log(mango.getEmail()); // mango@mail.com
+//   mango.changeEmail('mango@supermail.com');
+//   console.log(mango.getEmail()); // mango@supermail.com
+
+
+
+
+
+
 // задача 7
 // задача 8
 // задача 9
